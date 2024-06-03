ea0210697cc7e65a4539eed3eb6b6774340b8c26
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-06-03 17:39:10 +0100

    add example

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,23 @@ import EditorComparison from './EditorComparison';
 function App() {
   const [strand, setStrand] = React.useState(1)
   const [circular, setCircular] = React.useState(false)
+  const [useTidyUpSequenceData, setUseTidyUpSequenceData] = React.useState(false)
   const onButton1Click = () => {
     setStrand((s) => -s)
   }
   const onButton2Click = () => {
     setCircular((c) => !c)
   }
+  const onButton3Click = () => {
+    setUseTidyUpSequenceData((u) => !u)
+  }
 
   return (
     <>
       <button onClick={onButton1Click}>Change strand</button>
       <button onClick={onButton2Click}>Change linear/circular</button>
-      <EditorComparison strand={strand} circular={circular} />
+      <button onClick={onButton3Click}>{!useTidyUpSequenceData && 'not' } using tidyUpSequenceData</button>
+      <EditorComparison strand={strand} circular={circular} useTidyUpSequenceData={useTidyUpSequenceData} />
     </>
   );
 }
--- src/EditorComparison.jsx
+++ src/EditorComparison.jsx
@@ -3,28 +3,39 @@ import { SimpleCircularOrLinearView } from '@teselagen/ove';
 import { createVectorEditor } from '@teselagen/ove';
 import defaultMainEditorProps from './defaultMainEditorProps';
 import { tidyUpSequenceData } from '@teselagen/sequence-utils';
+import { genbankToJson } from '@teselagen/bio-parsers';
 
 
-function EditorComparison({ strand, circular }) {
-    const seq = tidyUpSequenceData({
-        circular,
-        sequence: "gagagagag",
-        features: [{ id: "lalala", name: "Mr Feature", start: 1, end: 5, strand }]
-    })
+function EditorComparison({ strand, circular, useTidyUpSequenceData }) {
+    // Read a genbank file and display it in the main editor
+    const { parsedSequence } = genbankToJson(`LOCUS       Untitled_Sequence           9 bp    DNA     circular SYN 03-JUN-2024
+    FEATURES             Location/Qualifiers
+         misc_feature    complement(2..6)
+                         /label="Mr Feature"
+    ORIGIN      
+            1 gagagagag     
+    //`)[0];
+    // Change the strand and circularity of the sequence based on props
+    parsedSequence.circular = circular;
+    parsedSequence.features[0].strand = strand;
+    // Use / not use tidyUpSequenceData based on props
+    const processedSequence = useTidyUpSequenceData ? tidyUpSequenceData(parsedSequence) : parsedSequence;
+
+    // Rendering code
     const nodeRef = React.useRef(null);
     React.useEffect(() => {
         const editorProps = {
-            sequenceData: seq,
+            sequenceData: processedSequence,
             ...defaultMainEditorProps,
         };
         const editor = createVectorEditor(nodeRef.current, { editorName: 'mainEditor', height: '800' });
         editor.updateEditor(editorProps);
-    }, [seq]);
+    }, [parsedSequence, strand, circular]);
 
     return (
         <div className="App">
             <SimpleCircularOrLinearView
-                sequenceData={seq}
+                sequenceData={processedSequence}
                 editorName="previewEditor"
                 annotationLabelVisibility={{
                     //set visibilities as you please
